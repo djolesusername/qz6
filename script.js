6dd6a90cb81b07b7681f9b2176570270c0ec13e9
Djordje <djole001@gmail.com>
2021-11-03 18:15:17 +0100

    desktop

--- script.js
+++ script.js
@@ -11,18 +11,40 @@ function handleClick(radio) {
   radio.parentElement.parentElement.lastElementChild.classList.add("optional-visible");
 }
 
+const updateSlidebar = (pledgeAmount) => {
+  //Split working only for up to 4-6 digit gotta work on that
+  const currentAmountString = document.getElementById("currentAmount").firstChild.textContent.split("$")[1];
+  let indexComma = currentAmountString.indexOf(",");
+  let currentAmount = Number(currentAmountString.slice(0, indexComma) + currentAmountString.slice(indexComma + 1));
+  let newAmount = currentAmount + Number(pledgeAmount);
+  let newAmountString = "$" + newAmount;
+  newAmountString = newAmountString.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById("currentAmount").firstChild.textContent = newAmountString;
+  //Split working only for up to 4-6 digit gotta work on that
+  console.log(document.getElementById("currentBakers"));
+  const currentBakers = document.getElementById("currentBakers").innerHTML;
+  let indexCommaB = currentBakers.indexOf(",");
+  let currentAmountB = Number(currentBakers.slice(0, indexCommaB) + currentBakers.slice(indexCommaB + 1));
+  let newAmountB = String(currentAmountB + 1).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById("currentBakers").firstChild.textContent = newAmountB;
+};
+
 // Get the modal
 var modal = document.getElementById("myModal");
+var modalS = document.getElementById("modalS");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var btn2 = document.getElementById("myBtn2");
 var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementsByClassName("tbc")[0];
+var closeModal = document.getElementsByClassName("close-modal")[0];
+//var mdBtn2 = document.getElementById("modalEnd2");
 
 // Get the <span> element that closes the modal
 //var span = document.getElementsByClassName("close")[0];
 var span = document.getElementById("close");
-console.log(span);
+var spanS = document.getElementById("closeS");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -34,15 +56,56 @@ btn2.onclick = function () {
 btn3.onclick = function () {
   modal.style.display = "block";
 };
+btn4.onclick = function () {
+  modal.style.display = "block";
+};
+closeModal.onclick = function () {
+  modalS.style.display = "none";
+};
+
+document.querySelector("#modalEnd1").addEventListener(
+  "click",
+  function (event) {
+    event.preventDefault();
+    let pledgeAmount = event.originalTarget.previousElementSibling.firstChild.value;
+    updateSlidebar(pledgeAmount);
+    modal.style.display = "none";
+    modalS.style.display = "block";
+  },
+  false
+);
+
+document.querySelector("#modalEnd2").addEventListener(
+  "click",
+  function (event) {
+    event.preventDefault();
+    let pledgeAmount = event.originalTarget.previousElementSibling.firstChild.value;
+    updateSlidebar(pledgeAmount);
+    modalS.style.display = "block";
+    modal.style.display = "none";
+  },
+  false
+);
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
-  console.log("clicked");
+};
+
+spanS.onclick = function () {
+  modalS.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function (event) {
+  if (event.target == modalS) {
+    modalS.style.display = "none";
+  }
+
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
+const bookmarkButton = document.getElementsByClassName("bookmarktext")[0];
+console.log(bookmarkButton.innerText);
